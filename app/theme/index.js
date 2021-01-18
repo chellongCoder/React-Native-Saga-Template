b854533c6a4608672bc34089f6f3b255969dd373
longnn <longnn17.developer@gmail.com>
2021-01-18 22:55:49 +0700

    initial commit 🐧

---
+++ app/theme/index.js
@@ -0,0 +1,11 @@
+import { Appearance } from 'react-native';
+
+import LightTheme from './light-theme';
+import DarkTheme from './dark-theme';
+
+const colorScheme = Appearance.getColorScheme();
+const theme = colorScheme === 'dark' ? DarkTheme : LightTheme;
+console.log('====================================');
+console.log('colorScheme', colorScheme);
+console.log('====================================');
+export { theme };
