7e1ba4a4b7a5f2bd63b20ef54926df56d71709e7
Lê Đình Hiếu <wanderlust252@gmail.com>
2021-04-11 18:07:17 +0700

    dm Long

--- app/components/search-bar/index.tsx
+++ app/components/search-bar/index.tsx
@@ -1,17 +1,17 @@
 import React, { useCallback, useRef } from 'react';
-import { TextInput, View } from 'react-native';
+import { TextInput, View, ViewStyle } from 'react-native';
 import { theme } from '../../theme';
 import { TextField } from '../text-field';
 import { useStyleSearch } from './styles';
 
-export const SearchBar = () => {
+export const SearchBar = ({ style }: { style: ViewStyle }) => {
   const { colors } = theme;
   const styles = useStyleSearch();
   const refInput = useRef<TextInput>(null);
 
   const onChangeTextSearch = useCallback(() => {}, []);
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, style]}>
       <TextField
         style={styles.input}
         placeholder={'Nhập nội dung tìm kiếm'}
