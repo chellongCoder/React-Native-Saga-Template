aca213c5e80d654e1d076559ceaf96f464203f22
longnn <longnn17.developer@gmail.com>
2021-05-20 15:25:03 +0700

    TODO: delete redux

--- app/redux/types/index.js
+++ app/redux/types/index.js
@@ -1,6 +1,1 @@
 export * from './auth.types';
-export * from './home.types';
-export * from './product.type';
-export * from './qr.types';
-export * from './account.types';
-export * from './notification.type';
--- app/redux/types/notification.type.ts
+++
@@ -1,3 +0,0 @@
-export const NOTIFICATION_REQUEST = '@@redux/notification/NOTIFICATION_REQUEST';
-export const NOTIFICATION_SUCCESS = '@@redux/notification/NOTIFICATION_SUCCESS';
-export const NOTIFICATION_FAILED = '@@redux/notification/NOTIFICATION_FAILED';
